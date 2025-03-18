9a23ec74a6e62ba7812a52c968a3bfd964afab4f
Sockalingamvenuka <154747063+Sockalingamvenuka@users.noreply.github.com>
2025-03-18 19:13:02 +0000

    print odd numbers using if function

---
+++ Odd.java
@@ -0,0 +1,24 @@
+ public class Odd{
+public static void main(String[] args){
+System.out.println("print the odd nubers");
+int i;
+for(i=1;i<=20;i++){
+	if(i%2!=0){
+System.out.println(i);
+	}
+}
+}
+}
+/*
+print the odd nubers
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+*/
